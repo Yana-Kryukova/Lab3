cf2e06efcc65addc663dd9c18da3c161ddcc487e
Yana-Kryukova <krjanik@icloud.com>
2023-05-01 11:57:02 +0700

    first

---
+++ test/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+Console.WriteLine("Enter A, B, +, -, * or /");
+string s = Console.ReadLine();
+int A=0, B=0;
+switch (s)
+{
+    case "A": A = int.Parse(Console.ReadLine());break;
+    case "B": B = int.Parse(Console.ReadLine()); break;
+    case "+":break;
+    case "-": break;
+    case "*": break;
+    case "/": break;
+}
